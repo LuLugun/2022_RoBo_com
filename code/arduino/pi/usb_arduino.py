1e3cccdc2df4062200d3788fc3c9f17d053bcc87
A7215455林群峰 <53947178+LuLugun@users.noreply.github.com>
2022-09-01 14:09:05 +0800

    Create usb_arduino.py

---
+++ code/arduino/pi/usb_arduino.py
@@ -0,0 +1,27 @@
+import serial
+
+ser1 = serial.Serial('/dev/ttyACM0', 9600)
+
+ser1.write('work'.encode()) # 對arduino輸入work
+output = ser1.readline().decode('utf-8').rstrip() #讀取樹梅派輸出
+
+
+# 網路範例
+
+import serial
+
+Port = "/dev/ttyUSB1"  # 串口
+baudRate = 9600  # 波特率
+ser = serial.Serial(Port, baudRate, timeout=1)
+
+while True:
+    send = '1'  # 发送给arduino的数据
+    ser.write(send.encode())
+    str = ser.readline().decode()  # 获取arduino发送的数据
+    if(str != ""):
+        print(str)
+        if(str == 'ok\r\n'): # 发送一次便退出
+            print('收到')
+            break
+
+ser.close()
