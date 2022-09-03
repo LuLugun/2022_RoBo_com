de15a3bdf227170ed55ddc94a591104b50eb79f1
A7215455林群峰 <53947178+LuLugun@users.noreply.github.com>
2022-09-03 23:10:26 +0800

    Add files via upload

---
+++ code/arduino/pi/button_change_robo_state.py
@@ -0,0 +1,34 @@
+import serial
+import time
+import requests
+
+Port = "/dev/ttyACM0"  # 串口
+baudRate = 9600  # 波特率
+ser = serial.Serial(Port, baudRate, timeout=1)
+bluetoothSerial = serial.Serial("/dev/rfcomm0",baudrate=38400)
+
+while True:
+    str = ser.readline().decode()  # 获取arduino发送的数据
+    if(str != ""):
+        if(str == 'btnOn\r\n'): # 发送一次便退出
+            url = "http://86e0-27-52-33-129.ngrok.io/put/robo_state/2"
+            response = requests.request("PUT", url)
+            response = response.json()
+            if response['robo_state'] == 2:
+                print('狀態改變成功:working')
+                bluetoothSerial.write(b'btnOn\r\n')
+            else:
+                print('狀態改變失敗')
+            
+        if(str == 'btnOff\r\n'): # 发送一次便退出
+            url = "http://86e0-27-52-33-129.ngrok.io/put/robo_state/1"
+            response = requests.request("PUT", url)
+            response = response.json()
+            if response['robo_state'] == 1:
+                print('狀態改變成功:maintain')
+                bluetoothSerial.write(b'btnOff\r\n')
+            else:
+                print('狀態改變失敗')
+ser.close()
+
+
